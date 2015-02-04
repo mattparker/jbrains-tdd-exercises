f8e97e244d065340e592cbb280c561b3d0e8252e
Matt Parker <matt@lamplightdb.co.uk>
2015-02-04 19:36:08 +0000

    Reorder test list - need output

--- session1/TillTest.php
+++ session1/TillTest.php
@@ -18,13 +18,15 @@ class TillTest extends PHPUnit_Framework_TestCase {
 
     }
 
+    public function test_we_output_the_price_when_we_receive_a_barcode () {}
+
     public function test_barcodes_are_not_too_short () {}
 
     public function test_barcodes_are_not_too_long () {}
 
     public function test_barcodes_are_numbers_only () {}
 
-    public function test_we_output_the_price_when_we_receive_a_barcode () {}
+
 
 
 }
