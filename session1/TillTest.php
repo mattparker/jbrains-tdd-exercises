e0b5445c3140a6b67694791fed496503c9c6efe8
Matt Parker <matt@lamplightdb.co.uk>
2015-02-04 19:32:48 +0000

    Initial test list

---
+++ session1/TillTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * Class TillTest
+ *
+ */
+class TillTest extends PHPUnit_Framework_TestCase {
+
+
+    public function test_instance () {}
+
+    public function test_we_can_send_a_barcode () {}
+
+    public function test_barcodes_are_not_too_short () {}
+
+    public function test_barcodes_are_not_too_long () {}
+
+    public function test_barcodes_are_numbers_only () {}
+
+    public function test_we_output_the_price_when_we_receive_a_barcode () {}
+
+
+}
+
