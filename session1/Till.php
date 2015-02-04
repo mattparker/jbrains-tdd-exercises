a2761a813af8058b5750a75260c24a06466bf15d
Matt Parker <matt@lamplightdb.co.uk>
2015-02-04 19:34:25 +0000

    Passing test 1 - instance

---
+++ session1/Till.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matt
+ * Date: 04/02/15
+ * Time: 19:29
+ */
+
+
+/**
+ * Class Till
+ *
+ * First exercise for jbrains TDD session - the Point Of Sale Exercise
+ *
+ */
+class Till {
+
+
+}
