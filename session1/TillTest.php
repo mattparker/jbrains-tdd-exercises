0303db0546fed7f0af0810bf3e3c5eedd7b2c1df
Matt Parker <matt@lamplightdb.co.uk>
2015-02-04 20:13:38 +0000

    Deleting redundant tests

--- session1/TillTest.php
+++ session1/TillTest.php
@@ -8,16 +8,6 @@ require_once 'Till.php';
 class TillTest extends PHPUnit_Framework_TestCase {
 
 
-    public function test_instance () {
-        new Till();
-    }
-
-    public function test_we_can_send_a_barcode () {
-        $till = new Till();
-        $till->onBarcode("1234567890");
-
-    }
-
     public function test_we_output_the_price_when_we_receive_a_barcode () {
         $till = new Till();
         $till->onBarcode("1234567890");
